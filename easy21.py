209be46a96064f9a633a8fd65130665ed3a36832
Zeynep <zeynep.gerem@tum.de>
2025-01-26 00:38:28 +0000

    first version with bugs

--- easy21.py
+++ easy21.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+from collections import defaultdict
+import matplotlib.pyplot as plt
 # inputs: 
 # a tuple of ints for state: dealer’s first card 1-10 and the player’s sum 1-21
 # a string for action: hit or stick
@@ -12,26 +14,39 @@ class Easy21():
         self.dealers_card = random.randint(1, 10)
         self.players_card = random.randint(1, 10)
         self.terminal_state = False
-        return (self.dealers_card, self.players_card), self.terminal_state
+        return self.dealers_card, self.players_card # (self.dealers_card, self.players_card), self.terminal_state
     
     def step(self, state, action):
-        dealers_sum = state[0][0]
-        players_sum = state[0][1]
+        dealers_sum = state[0]
+        players_sum = state[1]
         reward = 0
         if action == 'stick':
             # TODO: play out the dealer’s cards and return the final reward and terminal state
-            dealers_sum, reward = self.playout(dealers_sum, players_sum)
+            while dealers_sum < 17:
+                dealers_sum += self.sample_card()
             self.terminal_state = True
+            if dealers_sum>21 or players_sum>dealers_sum:
+                reward = 1
+            elif dealers_sum > players_sum:
+                reward = -1
+            elif dealers_sum == players_sum:
+                reward = 0
+            else:
+                print(f"missing condition!")
+
         elif action == 'hit':
             players_sum += self.sample_card()
             if players_sum > 21:
                 reward = -1
                 self.terminal_state = True
+            if players_sum == 21:
+                reward = 1
+                self.terminal_state = True
         else:
             print(f'unknowm action')
             return
-        next_state = (dealers_sum, players_sum)
-        return next_state, reward, self.terminal_state
+        next_state = (self.dealers_card, players_sum)
+        return next_state, reward
     
     # choose between -1 (p=1/3) and +1 (p=2/3) 
     def sample_card(self):
@@ -39,21 +54,7 @@ class Easy21():
         number = random.randint(1, 10)
         return int(color*number)
     
-    def playout(self, dealers_sum, players_sum):
-        while dealers_sum < 17:
-                print(dealers_sum)
-                dealers_sum += self.sample_card()
-        if dealers_sum >21 or dealers_sum < players_sum:
-            # TODO: update next state
-            reward = 1
-        elif dealers_sum == players_sum: # draw
-            reward = 0
-        elif dealers_sum == 21 or dealers_sum > players_sum:
-            # TODO: update next state
-            reward = -1
-        else:
-            print(f'Unknown state occurred with dealers sum:{dealers_sum}, players sum:{players_sum}')
-        return dealers_sum, reward
+   
 """ 
 Apply Monte-Carlo control to Easy21. Initialise the value function to zero. Use
 a time-varying scalar step-size of alphat = 1/N(st, at) and an epsilon-greedy exploration
@@ -69,31 +70,42 @@ taken from Sutton and Barto’s Blackjack example.
 """
 def MCControl(episodes=100, gamma=1, N_0 = 100):
     actions = ["hit", "stick"]
-    Q = {} # state-action value function 
-    N = {} # number of times a state-action pair visited
+    Q = defaultdict(int) # state-action value function 
+    N = defaultdict(lambda: N_0) # number of times a state-action pair visited. a dictionary of tuples
     returns = {} # returns per state-action pair
     env = Easy21()
     for episode in range(episodes):
-        state, terminal_state = env.reset()
+        state = env.reset()
         episode_memory = []
         episode_return = 0
-        while not terminal_state:
+
+        while not env.terminal_state:
             
             eps_t = N_0/(N_0 + N[(state,"hit")]+ N[(state,"stick")])
             # TODO: choose action
             is_greedy = np.random.choice([False,True],  p=[eps_t, 1-eps_t])
             if is_greedy:
-                action = max((action for (s, a) in Q if s == state),
-                                key=lambda action: Q[(state, action)])
+                try:
+                    action = max((action for (s, a) in Q if s == state),
+                                    key=lambda action: Q[(state, action)]) # if state is not visited before
+                    
+                except:
+                    action = np.random.choice(["hit","stick"],  p=[0.5, 0.5]) # choose randomly
             else:
                 action = np.random.choice(["hit","stick"],  p=[0.5, 0.5])
             N[(state,action)]+=1
             
             # update state
-            state, reward, terminal_state = env.step(state, action)
+            state, reward = env.step(state, action)
+            print(state, reward)
             episode_return += reward
             alpha_t = 1/N[(state,action)] # update at the end of episode
             Q[(state, action)] = Q[(state, action)] + alpha_t*(episode_return-Q[(state, action)])
     return Q
 
-Q = MCControl(episodes=100, gamma=1, N_0 = 100)
+def plot_optimal_value_function(Q):
+    print
+
+Q = MCControl(episodes=1000, gamma=1, N_0 = 100)
+# print(Q)
+plot_optimal_value_function(Q)
