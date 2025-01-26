c769d6e8755600cd211041f03f5478e5aa79b5d6
Zeynep <zeynep.gerem@tum.de>
2025-01-26 22:01:19 +0000

    debugging

--- easy21.py
+++ easy21.py
@@ -14,12 +14,14 @@ class Easy21():
         self.dealers_card = random.randint(1, 10)
         self.players_card = random.randint(1, 10)
         self.terminal_state = False
+        print(f'dealers card: {self.dealers_card}, players card: {self.players_card}')
         return self.dealers_card, self.players_card # (self.dealers_card, self.players_card), self.terminal_state
     
     def step(self, state, action):
         dealers_sum = state[0]
         players_sum = state[1]
         reward = 0
+        
         if action == 'stick':
             # TODO: play out the dealer’s cards and return the final reward and terminal state
             while dealers_sum < 17:
@@ -45,12 +47,14 @@ class Easy21():
         else:
             print(f'unknowm action')
             return
+        print(action, players_sum)
         next_state = (self.dealers_card, players_sum)
         return next_state, reward
     
     # choose between -1 (p=1/3) and +1 (p=2/3) 
     def sample_card(self):
         color = np.random.choice([-1,1],  p=[1/3, 2/3])
+        # print(f'color:{color}')
         number = random.randint(1, 10)
         return int(color*number)
     
@@ -63,15 +67,14 @@ the number of times that state s has been visited, and N(s, a) is the number
 of times that action a has been selected from state s. Feel free to choose an
 alternative value for N0, if it helps producing better results. Plot the optimal
 value function V
-∗
-(s) = maxa Q∗
+(s) = maxa 
 (s, a) using similar axes to the following figure
 taken from Sutton and Barto’s Blackjack example.
 """
 def MCControl(episodes=100, gamma=1, N_0 = 100):
     actions = ["hit", "stick"]
-    Q = defaultdict(int) # state-action value function 
-    N = defaultdict(lambda: N_0) # number of times a state-action pair visited. a dictionary of tuples
+    Q = defaultdict(lambda: defaultdict(float))# {}# defaultdict(int) # state-action value function 
+    N = defaultdict(lambda: 1e-3) # number of times a state-action pair visited. a dictionary of tuples
     returns = {} # returns per state-action pair
     env = Easy21()
     for episode in range(episodes):
@@ -86,26 +89,32 @@ def MCControl(episodes=100, gamma=1, N_0 = 100):
             is_greedy = np.random.choice([False,True],  p=[eps_t, 1-eps_t])
             if is_greedy:
                 try:
-                    action = max((action for (s, a) in Q if s == state),
-                                    key=lambda action: Q[(state, action)]) # if state is not visited before
-                    
+                    action = max((action for s[a] in Q if s == state),
+                                    key=lambda action: Q[state][action]) # if state is not visited before
                 except:
                     action = np.random.choice(["hit","stick"],  p=[0.5, 0.5]) # choose randomly
+                    # Q[state][action] = 0
+                    # print(Q[state][action])
+                
             else:
                 action = np.random.choice(["hit","stick"],  p=[0.5, 0.5])
             N[(state,action)]+=1
             
+            # print(f'chosen action: {action}')
             # update state
-            state, reward = env.step(state, action)
-            print(state, reward)
+            next_state, reward = env.step(state, action)
+            # print(state, action, reward)
             episode_return += reward
             alpha_t = 1/N[(state,action)] # update at the end of episode
-            Q[(state, action)] = Q[(state, action)] + alpha_t*(episode_return-Q[(state, action)])
+            Q[state][action] = Q[state][action] + alpha_t*(episode_return-Q[state][action])
+            # print(state, action, reward, Q[state][action])
+            state = next_state
+        print(state, action, reward, Q[state][action])
     return Q
 
 def plot_optimal_value_function(Q):
     print
 
-Q = MCControl(episodes=1000, gamma=1, N_0 = 100)
+Q = MCControl(episodes=100, gamma=1, N_0 = 100)
 # print(Q)
 plot_optimal_value_function(Q)
