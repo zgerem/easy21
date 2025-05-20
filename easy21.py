d9891932a7d6eeb96346c39e54b780b883398837
Zeynep <zeynep.gerem@tum.de>
2025-05-20 23:12:32 +0100

    MC Control first plot

--- easy21.py
+++ easy21.py
@@ -3,6 +3,7 @@ import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import pdb
+import itertools
 # inputs: 
 # a tuple of ints for state: dealer’s first card 1-10 and the player’s sum 1-21
 # a string for action: hit or stick
@@ -54,7 +55,7 @@ class Easy21():
     
     # choose between -1 (p=1/3) and +1 (p=2/3) 
     def sample_card(self):
-        color = 1 # np.random.choice([-1,1],  p=[1/3, 2/3])
+        color = np.random.choice([-1,1],  p=[1/3, 2/3])
         # print(f'color:{color}')
         number = random.randint(1, 10)
         return int(color*number)
@@ -80,63 +81,76 @@ def MCControl(episodes=100, gamma=1, N_0 = 100):
     env = Easy21()
     for episode in range(episodes):
         state = env.reset()
-        episode_memory = set()
-        episode_return = 0
+        episode_memory = []
+        episode_rewards = []
 
         while not env.terminal_state:
             
             eps_t = N_0/(N_0 + N[(state,"hit")]+ N[(state,"stick")]) # for eps greedy exploration. in time, eps_t becomes less and less
             # TODO: choose action
             is_greedy = np.random.choice([False,True],  p=[eps_t, 1-eps_t]) # with decreasing eps, exploration decreases 
-            if is_greedy: # if greedy choose the action producing highest value
-                # pdb.set_trace()
-                # print('it is greedy now!')
-                # try:
+            if is_greedy: # if greedy, choose the action producing highest value
+                # print(f'it is greedy now in episode: {episode}!')
+                
                 if 'hit' not in Q[state]: Q[state]['hit']==0
                 if 'stick' not in Q[state]: Q[state]['stick']==0
                 action = max((a for a in Q[state]), key=lambda a: Q[state][a])
-                # except: # if state is not visited before
-                # action = np.random.choice(["hit","stick"],  p=[0.5, 0.5]) # choose randomly
-                # Q[state][action] = 0
-                # print(Q[state][action])
                 
             else:
                 action = np.random.choice(["hit","stick"],  p=[0.5, 0.5])
-            N[(state,action)]+=1
             
             # print(f'chosen action: {action}')
             # update state
             next_state, reward = env.step(state, action)
-            # print(state, action, reward)
-            episode_return += reward
-            alpha_t = 1/N[(state,action)] # update at the end of episode
-            Q[state][action] = Q[state][action] + alpha_t*(episode_return-Q[state][action])
-            episode_memory.add((state, action, reward))
-            # print(state, action, reward, Q[state][action])
+            episode_rewards.append(reward)
+            episode_memory.append((state, action))
             state = next_state
-        if action == 'hit':
-            pass
-            # print(state, action, reward, Q[state][action])
+        episode_return = 0
+        seen_steps = []
+        for i in range(len(episode_memory)):
+            state, action = episode_memory[i]
+            if (state,action) in seen_steps:
+                continue
+            seen_steps.append((state, action))
+            episode_return += episode_rewards[i]
+            alpha_t = 1/N[(state,action)] # update at the end of episode
+            N[(state,action)]+=1
+            Q[state][action] = Q[state][action] + alpha_t*(episode_return-Q[state][action])
+        
     return Q
 
 def plot_optimal_value_function(Q):
-    dealers_showing = []
-    players_sum = []
-    for key, value in Q.items():
-        dealers_showing.append(key[0])
-        players_sum.append(key[1])
-        # print(key,value)
-    ax = plt.axes(projection ='3d')
+    dealers_showing = [i for i in range(1,11)]
+    players_sum = [i for i in range(12,21)]
     
-    value_func = np.ones(len(players_sum))
-    # plotting
-    ax.plot3D(dealers_showing, players_sum, value_func, 'green')
-    ax.set_title('3D line plot geeks for geeks')
+    X = []
+    Y = []
+    Z = []
+
+    for dealer, player in itertools.product(dealers_showing, players_sum):
+        best_value = max(Q[(dealer, player)].values())
+        X.append(dealer)
+        Y.append(player)
+        Z.append(best_value)
+    
+    X = np.array(X)
+    Y = np.array(Y)
+    Z = np.array(Z)
+
+    # Plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(X, Y, Z, cmap='viridis')
+
+    ax.set_xlabel('Dealer Showing')
+    ax.set_ylabel('Player Sum')
+    ax.set_zlabel('Best Value')
+    ax.set_title('Optimal Value Function (Best Action Value per State)')
+
     plt.show()
 
 def TDLearning(episodes=10000, gamma=1, N_0 = 100):
     pass
 if __name__ == "__main__":
-    Q = MCControl(episodes=int(1e6), gamma=1, N_0 = 100) # agent uses look up table 
-    print(Q)
+    Q = MCControl(episodes=int(1e5), gamma=1, N_0 = 100) # agent uses look up table 
     plot_optimal_value_function(Q)
