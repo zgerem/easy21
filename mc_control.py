13eb69303c9c66dd6b3442c2a63c9539926ee196
Zeynep <zeynep.gerem@tum.de>
2025-05-22 22:27:55 +0100

    organise files

---
+++ mc_control.py
@@ -0,0 +1,56 @@
+import random
+import numpy as np
+from collections import defaultdict
+import pdb
+import itertools
+import easy21
+
+def MCControl(episodes=100, gamma=1, N_0 = 100):
+    actions = ["hit", "stick"]
+    Q = defaultdict(lambda: defaultdict(float))# {}# defaultdict(int) # state-action value function 
+    N = defaultdict(lambda: 1e-5) # number of times a state-action pair visited. a dictionary of tuples
+    env = easy21.Easy21()
+    for episode in range(episodes):
+        state = env.reset()
+        episode_memory = []
+        episode_rewards = []
+
+        while not env.terminal_state:
+            
+            eps_t = N_0/(N_0 + N[(state,"hit")]+ N[(state,"stick")]) # for eps greedy exploration. in time, eps_t becomes less and less
+            # TODO: choose action
+            is_greedy = np.random.choice([False,True],  p=[eps_t, 1-eps_t]) # with decreasing eps, exploration decreases 
+            if is_greedy: # if greedy, choose the action producing highest value
+                # print(f'it is greedy now in episode: {episode}!')
+                
+                if 'hit' not in Q[state]: Q[state]['hit']=0
+                if 'stick' not in Q[state]: Q[state]['stick']=0
+                action = max((a for a in Q[state]), key=lambda a: Q[state][a])
+                
+            else:
+                action = np.random.choice(["hit","stick"],  p=[0.5, 0.5])
+            
+            # print(f'chosen action: {action}')
+            # update state
+            next_state, reward = env.step(state, action)
+            episode_rewards.append(reward)
+            episode_memory.append((state, action))
+            state = next_state
+        episode_return = 0
+        seen_steps = []
+        for i in reversed(range(len(episode_memory))):
+            state, action = episode_memory[i]
+            
+            if (state,action) in seen_steps:
+                continue
+            episode_return = gamma*episode_return+episode_rewards[i]
+            seen_steps.append((state, action))
+            alpha_t = 1/N[(state,action)] # update at the end of episode
+            N[(state,action)]+=1
+            Q[state][action] = Q[state][action] + alpha_t*(episode_return-Q[state][action])
+        
+    return Q
+
+if __name__ == "__main__":
+    Q = MCControl(episodes=int(1e6), gamma=1, N_0 = 100) # agent uses look up table 
+    
